ec806c31b88ab434e9a71ecca6c38745ee744ca6
飞酱 <zjf111435@alibaba-inc.com>
2017-03-30 16:35:54 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # electron-hotkey
+
+Manage shortcuts in Electron.
+
+You can use it in main or renderer process.
+
+## Usage
+
+```javascript
+npm install --save electron-hotkey
+
+// in main process
+const hotkey = require('electron-hotkey');
+
+// in renderer process
+const hotkey = require('electron').remote.require('electron-hotkey');
+```
+
+**register(typeOrWindow, accelerator, eventName)**
+
+typeOrWindow: 'global', 'local' or a window instance.
+
+```javascript
+hotkey.register('global', 'CommandOrControl+1', 'event-1');
+hotkey.register('local', 'CommandOrControl+2', 'event-2');
+hotkey.register(myWindow, 'CommandOrControl+3', 'event-3');
+
+app.on('shortcut-pressed', (event) => {
+    console.log(event);
+});
+```
+
+**unregister(accelerator)**
+
+**unregisterAll()**
+
+**isRegistered(accelerator)**
+
+Return value could be false, 'global', 'local', or a window instance.
+
+**load(config)**
+
+A config example could be:
+
+```javascript
+const config = [
+    {type: 'global', accelerator: 'CommandOrControl+1', event: 'event-1'},
+    {type: 'local', accelerator: 'CommandOrControl+2', event: 'event-2'},
+    {type: myWindow, accelerator: 'CommandOrControl+3', event: 'event-3'},
+];
+```
--- index.js
+++ index.js
@@ -1,1 +1,75 @@
-module.exports = {};
+const { app, BrowserWindow, globalShortcut } = require('electron');
+const localShortcut = require('electron-localshortcut');
+
+if (process.type !== 'browser') {
+    console.error('Please use remote.require to import electron-hotkey in renderer process.');
+}
+
+function emit(eventName) {
+    app.emit('shortcut-pressed', eventName);
+}
+
+function register(typeOrWindow, accelerator, eventName) {
+    if (typeOrWindow === 'global' || typeOrWindow === 'g') {
+        globalShortcut.register(accelerator, () => emit(eventName));
+    } else if (typeOrWindow === 'local' || typeOrWindow === 'l') {
+        localShortcut.register(accelerator, () => emit(eventName));
+    } else {
+        localShortcut.register(typeOrWindow, accelerator, () => emit(eventName));
+    }
+}
+
+function unregister(accelerator) {
+    globalShortcut.unregister(accelerator);
+    localShortcut.unregister(accelerator);
+
+    BrowserWindow.getAllWindows().forEach((win) => {
+        localShortcut.unregister(win, accelerator);
+    });
+}
+
+function unregisterAll() {
+    globalShortcut.unregisterAll();
+    localShortcut.unregisterAll();
+
+    BrowserWindow.getAllWindows().forEach((win) => {
+        localShortcut.unregisterAll(win);
+    });
+}
+
+function isRegistered(accelerator) {
+    const isRegisteredInGlobal = globalShortcut.isRegistered(accelerator);
+    const isRegisteredInLocal = localShortcut.isRegistered(accelerator);
+    const isRegisteredInWindow = false;
+
+    BrowserWindow.getAllWindows().forEach((win) => {
+        if (localShortcut.isRegistered(win, accelerator)) {
+            isRegisteredInWindow = win;
+            return false;
+        }
+    });
+
+    if (isRegisteredInWindow) {
+        return isRegisteredInWindow;
+    } else if (isRegisteredInLocal) {
+        return 'local';
+    } else if (isRegisteredInGlobal) {
+        return 'global';
+    }
+
+    return false;
+}
+
+function load(configs) {
+    configs.forEach((config) => {
+        register(config.type, config.accelerator, config.event);
+    });
+}
+
+module.exports = {
+    register,
+    unregister,
+    unregisterAll,
+    isRegistered,
+    load,
+};
